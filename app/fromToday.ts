cc224dc62d3f1245b680fab6451cd076821623d6
August Gaukstad / Intility AS <august.gaukstad@intility.no>
2024-04-29 07:52:21 +0200

    fix: user select none, padding instead of margin

--- app/fromToday.ts
+++ app/fromToday.ts
@@ -1,6 +1,6 @@
 const fromToday = (currDate: Date, day: Date) => {
     const timeDiff = currDate.getTime() - day.getTime();
-    console.log(timeDiff / (1000 * 3600 * 24))
+    //console.log(timeDiff / (1000 * 3600 * 24))
 	const daysDiff = Math.trunc(timeDiff / (1000 * 3600 * 24));
     return daysDiff;
 }
